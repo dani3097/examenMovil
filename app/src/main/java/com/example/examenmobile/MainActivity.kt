3b90e6938e5e3722721185feac839e8e7e41b453
daniela vela <danielitavo97@gmail.com>
2020-04-30 17:49:35 -0400

    first commit

---
+++ app/src/main/java/com/example/examenmobile/MainActivity.kt
@@ -0,0 +1,12 @@
+cd Andr package com.example.examenmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
